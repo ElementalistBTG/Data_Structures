9e0f60e5a91bf840fc0ebb649db0b1453beedbc0
ElementalistBTG <elementalistbtg@gmail.com>
2021-10-15 08:35:05 +0300

    reverse function and mergeSortedArray function added

--- src/main/kotlin/TestClass.kt
+++ src/main/kotlin/TestClass.kt
@@ -1,15 +1,7 @@
 
 
 fun main() {
-    //input
 
-//    val array1: Array<Int?> = arrayOf(null)
-//    val array2: Array<Int?> = arrayOf(0, 2)
-//    val newArray = mergeSortedArrays(array1, array2)
-//
-//    for (index in newArray) {
-//        print(index)
-//    }
 }
 
 
