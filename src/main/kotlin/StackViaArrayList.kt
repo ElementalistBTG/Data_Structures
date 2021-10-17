7472ea0a8e55f7b933ed15cf6e7af3f352a6274a
ElementalistBTG <elementalistbtg@gmail.com>
2021-10-17 22:51:25 +0300

    added stack via arraylist

--- src/main/kotlin/StackViaArrayList.kt
+++ src/main/kotlin/StackViaArrayList.kt
@@ -72,4 +72,28 @@ fun <T : Any> LinkedList<T>.printInReverse2() {
         node = stack.pop()
     }
 }
+/** Test */
+/*
+    val stack = StackImpl<Int>().apply {
+        push(1)
+        push(2)
+        push(3)
+        push(4)
+    }
+    print(stack)
+    val poppedElement = stack.pop()
+    if (poppedElement != null) {
+        println("Popped: $poppedElement")
+    }
+    print(stack)
+    //list to stack
+    val list = listOf("A", "B", "C", "D")
+    val stack2 = StackImpl.create(list)
+    print(stack2)
+    println("Popped: ${stack2.pop()}")
+    //create a stack in a similar way as listOf()
+    val stack3 = stackOf(1.0, 2.0, 3.0, 4.0)
+    print(stack3)
+    println("Popped: ${stack3.pop()}")
+ */
 
