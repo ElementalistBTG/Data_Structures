271c62ab4897bab373fad7c4190a145735899cec
ElementalistBTG <elementalistbtg@gmail.com>
2021-10-25 00:29:30 +0300

    added stack via arraylist

--- src/main/kotlin/Algorithms/Recursion.kt
+++ src/main/kotlin/Algorithms/Recursion.kt
@@ -19,7 +19,7 @@ class recursionClass {
     }
 
     //fibonacci numbers 0, 1, 1, 2, 3, 5, 8, 13, 21
-    fun fibonacciIterative(n: Int): Int {
+    fun fibonacciIterative(n: Int): Int { //O(n)
         val array = arrayListOf<Int>(0,1)
         for(i in 2..n){
             array.add(array[i-2]+array[i-1])
@@ -27,7 +27,7 @@ class recursionClass {
         return array.last()//or array[n]
     }
 
-    fun fibonacciRecursive(n:Int): Int{
+    fun fibonacciRecursive(n:Int): Int{ //O(2^n)
 //        if(n==0) return 0
 //        else if(n==1) return 1
         if(n<2) return n
