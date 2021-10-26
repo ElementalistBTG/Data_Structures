8bb693e22ff6f9417e4140543500188b13af8c25
ElementalistBTG <elementalistbtg@gmail.com>
2021-10-26 11:22:56 +0300

    sorting algorithms

---
+++ src/main/kotlin/Algorithms/Sorting.kt
@@ -0,0 +1,19 @@
+package algorithms
+
+class SortingAlgorithms{
+
+    fun mergeSort(){
+
+    }
+
+    fun quickSort(){
+
+    }
+
+    fun selectionSort(){}
+
+    fun insertionSort(){}
+
+    fun bubbleSort(){}
+
+}
