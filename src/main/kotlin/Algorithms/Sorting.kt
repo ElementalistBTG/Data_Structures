4d16aaefc6da85e9fd754c22e15349b148abf8c0
ElementalistBTG <elementalistbtg@gmail.com>
2021-10-26 12:46:12 +0300

    sorting algorithms

--- src/main/kotlin/Algorithms/Sorting.kt
+++ src/main/kotlin/Algorithms/Sorting.kt
@@ -1,19 +1,90 @@
 package algorithms
 
-class SortingAlgorithms{
-
-    fun mergeSort(){
-
+class SortingAlgorithms {
+    fun customSort(array: Array<Int>): Array<Int> {
+        val arraySize = array.size
+        for (i in 0..arraySize - 2) {
+            for (j in i + 1 until arraySize) {
+                if (array[i] > array[j]) {
+                    val temp = array[j]
+                    array[j] = array[i]
+                    array[i] = temp
+                }
+            }
+        }
+        return array
     }
 
-    fun quickSort(){
-
+    fun bubbleSort(array: Array<Int>): Array<Int> {
+        val arraySize = array.size
+        for (i in 0 until arraySize - 1) {
+            for (j in 0 until arraySize - 1) {
+                if (array[j] > array[j + 1]) {
+                    val temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+                }
+            }
+        }
+        return array
     }
 
-    fun selectionSort(){}
+    fun selectionSort(array: Array<Int>): Array<Int> {
+        val arraySize = array.size
+        for (i in 0 until arraySize) {
+            var min = i
+            var temp = array[i]
+            for (j in i + 1 until arraySize) {
+                if (array[j] < array[min]) {
+                    min = j
+                }
+            }
+            array[i] = array[min]
+            array[min] = temp
+        }
+        return array
+    }
 
-    fun insertionSort(){}
+    //for arrays that are nearly sorted
+    fun insertionSort(array: Array<Int>): Array<Int> {
 
-    fun bubbleSort(){}
+        val arraySize = array.size
+        if (array.isEmpty() || arraySize < 2) {
+            return array
+        }
+
+        for (count in 0 until arraySize) {
+            val item = array[count]
+            var i = count
+            while (i > 0 && item < array[i - 1]) {
+                array[i] = array[i - 1]
+                i -= 1
+            }
+            array[i] = item
+        }
+        return array
+    }
+    //list implementation
+    fun insertionSort(items: MutableList<Int>): List<Int> {
+        if (items.isEmpty() || items.size < 2) {
+            return items
+        }
+        for (count in 1 until items.count()) {
+            // println(items)
+            val item = items[count]
+            var i = count
+            while (i > 0 && item < items[i - 1]) {
+                items[i] = items[i - 1]
+                i -= 1
+            }
+            items[i] = item
+        }
+        return items
+    }
+
+    fun mergeSort() {}
+
+    fun quickSort() {}
+
 
 }
--- src/main/kotlin/TestClass.kt
+++ src/main/kotlin/TestClass.kt
@@ -1,6 +1,12 @@
 fun main() {
-    val myRecursionClass = recursionClass()
 
-    println(myRecursionClass.fibonacciIterative(8))
+    val myNumberArray = arrayOf(99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0)
+
+    val sortingClass = algorithms.SortingAlgorithms()
+    val sortedArray = sortingClass.insertionSort(myNumberArray)
+    for (i in sortedArray) {
+        print(" $i")
+    }
+
 }
 
