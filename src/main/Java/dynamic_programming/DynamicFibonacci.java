c5cad16d03a2fdd11e09a3ff5fdcbd13664da3ee
ElementalistBTG <elementalistbtg@gmail.com>
2021-11-01 14:27:59 +0200

    java classes added

---
+++ src/main/Java/dynamic_programming/DynamicFibonacci.java
@@ -0,0 +1,23 @@
+package dynamic_programming;
+
+import java.util.HashMap;
+
+class DynamicFibonacciJava {
+
+    private static final HashMap<Integer, Integer> cache = new HashMap<>();
+
+    public static int fibonacciMaster(int n) {
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        }
+        if (n < 2) {
+            return n;
+        }
+        cache.put(n, fibonacciMaster(n - 1) + fibonacciMaster(n - 2));
+        return cache.get(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacciMaster(1000));
+    }
+}
