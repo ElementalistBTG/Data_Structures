e0eab8bd406e13ce99bef28c099a341461fa471d
ElementalistBTG <elementalistbtg@gmail.com>
2021-11-03 13:45:38 +0200

    MaximumDepthOfBinaryTree

---
+++ src/main/kotlin/Exercises/ContainerWithMostWater.kt
@@ -0,0 +1,26 @@
+package Exercises
+
+//similar to best time to buy stockss
+class ContainerWithMostWater {
+    fun maxArea(height: IntArray): Int {
+        var left = 0
+        var right = height.size - 1
+        var maxArea = 0
+
+        while (left < right) {
+            maxArea = Math.max(maxArea,
+                Math.min(height[left], height[right] * (right - left)))
+            if (height [left] < height[right])
+                left++
+            else
+                right--
+        }
+        return maxArea
+    }
+}//time O(n), space O(1)
+
+fun main(){
+    val myClass = ContainerWithMostWater()
+    val areas = intArrayOf(1,5,6,5,3,2,7,9,2,6)
+    println("max area is: ${myClass.maxArea(areas)}")
+}
